26425b502c59adfe5dbbb5f93f8c771dc09b91b9
Arun Kumar K <art@Aruns-MacBook-Pro.local>
2026-01-18 09:53:09 +0530

    style: increase header logo size to h-20 for better visibility

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -98,7 +98,7 @@ const Header: React.FC = () => {
               to="/"
               className="flex items-center space-x-2 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 rounded"
             >
-              <img src={logo} alt="ilovememe.in" className="h-14 w-auto rounded object-contain" />
+              <img src={logo} alt="ilovememe.in" className="h-20 w-auto rounded object-contain" />
             </Link>
           </div>
 
